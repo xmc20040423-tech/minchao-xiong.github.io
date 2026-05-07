afe3eab03572bef3720507a28ee206b9260875d3
Minchao Xiong <xmc20040423@gmail.com>
2026-05-06 22:33:19 -0600

    Add files via upload

---
+++ docs/quarto-search.js
@@ -0,0 +1,72 @@
+[
+  {
+    "objectID": "index.html",
+    "href": "index.html",
+    "title": "Minchao Xiong",
+    "section": "",
+    "text": "Hello! Welcome to my personal website."
+  },
+  {
+    "objectID": "index.html#skills",
+    "href": "index.html#skills",
+    "title": "Minchao Xiong",
+    "section": "Skills",
+    "text": "Skills\n\nPython Programming\nBasketball\nTennis"
+  },
+  {
+    "objectID": "index.html#education",
+    "href": "index.html#education",
+    "title": "Minchao Xiong",
+    "section": "Education",
+    "text": "Education\n\nUniversity of Colorado Denver\nMajor: Economics\nCourses: Data Wrangling and Econometrics"
+  },
+  {
+    "objectID": "index.html#basketball",
+    "href": "index.html#basketball",
+    "title": "Hobbies",
+    "section": "Basketball",
+    "text": "Basketball\nI enjoy playing basketball because it helps me stay active and work with teammates."
+  },
+  {
+    "objectID": "index.html#tennis",
+    "href": "index.html#tennis",
+    "title": "Hobbies",
+    "section": "Tennis",
+    "text": "Tennis\nI like tennis because it improves my focus and reaction speed. I also participated in competitions before."
+  },
+  {
+    "objectID": "index.html#fitness",
+    "href": "index.html#fitness",
+    "title": "Hobbies",
+    "section": "Fitness",
+    "text": "Fitness\nI enjoy going to the gym late at night because it helps me relax and clear my mind. Sometimes I cannot fall asleep until 2 or 3 AM, so working out alone at night has become part of my routine.The top photo is the one I took when I was working out."
+  },
+  {
+    "objectID": "hobbies.html",
+    "href": "hobbies.html",
+    "title": "Hobbies",
+    "section": "",
+    "text": "I enjoy playing basketball because it helps me stay active and work with teammates. \n\n\n\nI like tennis because it improves my focus and reaction speed. I also participated in competitions before.\n\n\n\nI enjoy going to the gym late at night because it helps me relax and clear my mind. Sometimes I cannot fall asleep until 2 or 3 AM, so working out alone at night has become part of my routine."
+  },
+  {
+    "objectID": "hobbies.html#basketball",
+    "href": "hobbies.html#basketball",
+    "title": "Hobbies",
+    "section": "",
+    "text": "I enjoy playing basketball because it helps me stay active and work with teammates."
+  },
+  {
+    "objectID": "hobbies.html#tennis",
+    "href": "hobbies.html#tennis",
+    "title": "Hobbies",
+    "section": "",
+    "text": "I like tennis because it improves my focus and reaction speed. I also participated in competitions before."
+  },
+  {
+    "objectID": "hobbies.html#fitness",
+    "href": "hobbies.html#fitness",
+    "title": "Hobbies",
+    "section": "",
+    "text": "I enjoy going to the gym late at night because it helps me relax and clear my mind. Sometimes I cannot fall asleep until 2 or 3 AM, so working out alone at night has become part of my routine."
+  }
+]
